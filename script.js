e073ec49a93fe683f4c271c4056c97895e96681c
liza-liz <elizalim036@gmail.com>
2025-12-09 17:01:43 +0800

    refactor return value of function computer choice

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3) + 1;
+    let number = Math.floor(Math.random() * 3) + 1;
+    console.log(number);
+    if (number === 1) {return "ROCK"}
+    else if (number === 2) {return "PAPER"}
+    else return "SCISSOR";
 }
 function getHumanChoice() {
-    return +prompt("Enter a number: ");
+    return prompt("Enter a choice: ");
 }
+function playRound(humanChoice, computerChoice){
+
+}
+let humanScore = 0, computerScore = 0;
